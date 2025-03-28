d8e38f31b206869c9338ec7db99ea5e1b5e8abff
Leon Arndt <leon.arndt@innogames.com>
2025-03-28 13:30:25 +0100

    add and remove from item inventory

--- Assets/Scripts/LeonsGeheimeScripts/InventoryState.cs
+++ Assets/Scripts/LeonsGeheimeScripts/InventoryState.cs
@@ -15,15 +15,47 @@ namespace Logic
         protected override void OnEndGame()
         {
         }
-        
+
+        public IReadOnlyDictionary<FinishedItemType, uint> Get()
+        {
+            return _items;
+        }
+
         private void Add(FinishedItemType type, uint amount)
         {
-            // TODO: implement
+            if (type == null || amount == 0)
+            {
+                return;
+            }
+
+            if (_items.TryGetValue(type, out _))
+            {
+                _items[type] += amount;
+            }
+            else
+            {
+                _items[type] = amount;
+            }
         }
 
         private bool TryRemove(FinishedItemType type, uint amount)
         {
-            // TODO: implement
+            if (type == null || amount == 0)
+            {
+                return true;
+            }
+            
+            if (_items.TryGetValue(type, out var currentAmount))
+            {
+                if (currentAmount >= amount)
+                {
+                    _items[type] = currentAmount - amount;
+                    return true;
+                }
+
+                return false;
+            }
+
             return false;
         }
     }
