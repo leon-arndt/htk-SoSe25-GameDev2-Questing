7f1538cefa57939cb0b6718030d168094523dd01
Leon Arndt <leon.arndt@innogames.com>
2025-04-25 13:03:01 +0200

    start mocking quest state

---
+++ Assets/Scripts/LeonsGeheimeScripts/FinishedQuestState.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using Logic;
+using UnityEngine;
+
+namespace LeonsGeheimeScripts
+{
+    public class FinishedQuestState : SaveState
+    {
+        private List<QuestState> _questStates = new();
+
+        public override void OnStartGame()
+        {
+            _questStates = new List<QuestState>();
+        }
+
+        public override void OnEndGame()
+        {
+        }
+
+        public void StartQuest(IQuest quest)
+        {
+            if (_questStates.Any(q => q.Quest.GetId() == quest.GetId()))
+            {
+                Debug.LogWarning($"Quest{quest.GetId()} already started - not starting it again");
+                return;
+            }
+            
+            var state = new QuestState()
+            {
+                Quest = quest,
+                Status = QuestStatus.Started,
+            };
+            _questStates.Add(state);
+            quest.OnQuestStart();
+            
+            Debug.Log("Quest " + quest.GetId() + " started");
+        }
+        
+        public struct QuestState
+        {
+            public IQuest Quest;
+            public QuestStatus Status;
+        };
+
+        public enum QuestStatus
+        {
+            Started = 0,
+            Completed = 1
+        }
+    }
+
+    public interface IQuest
+    {
+        string GetId();
+        void OnQuestStart();
+    }
+}
