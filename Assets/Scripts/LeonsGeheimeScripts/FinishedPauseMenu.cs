32863bbce0261e08d33d33a812e7a0475b424d4e
Leon Arndt <leon.arndt@innogames.com>
2025-07-18 13:07:51 +0200

    add finished options and pause menu flow

--- Assets/Scripts/LeonsGeheimeScripts/FinishedPauseMenu.cs
+++ Assets/Scripts/LeonsGeheimeScripts/FinishedPauseMenu.cs
@@ -1,67 +1,69 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-namespace FinishedScripts  
-{  
-    public class FinishedPauseMenu : MonoBehaviour  
-    {  
-        public GameObject pauseUi;  
-        public GameObject settingsUi;  
-        public Button settingsButton;  
-        public Button continueButton;  
-        public Button returnToPauseButton;  
-         
-        private void Awake()  
-        {  
-            settingsButton.onClick.AddListener(ActivateSettings);  
-            continueButton.onClick.AddListener(Continue);  
-            returnToPauseButton.onClick.AddListener(ActivatePauseMenu);  
-              
-            pauseUi.SetActive(false);  
-            settingsUi.SetActive(false);  
-        }  
-  
-        private void Update()  
-        {  
-            if (Input.GetKeyDown(KeyCode.Escape))  
-            {  
-                if (settingsUi.activeSelf)  
-                {  
-                    ActivatePauseMenu();  
-                }  
-                else if (pauseUi.activeSelf)  
-                {  
-                    Continue();  
-                }  
-                else  
-                {  
-                    ActivatePauseMenu();  
-                }  
-            }  
-        }  
-  
-        private void Continue()  
-        {  
-            pauseUi.SetActive(false);  
-            settingsUi.SetActive(false);  
-            Time.timeScale = 1f;  
-            Cursor.lockState = CursorLockMode.Locked;  
-            Cursor.visible = false;  
-        }  
-  
-        private void ActivateSettings()  
-        {  
-            pauseUi.SetActive(false);  
-            settingsUi.SetActive(true);  
-        }  
-  
-        private void ActivatePauseMenu()  
-        {  
-            settingsUi.SetActive(false);  
-            pauseUi.SetActive(true);  
-            Time.timeScale = 0f;  
-            Cursor.lockState = CursorLockMode.None;  
-            Cursor.visible = true;  
-        }  
-    }  
+namespace FinishedScripts
+{
+    public class FinishedPauseMenu : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference pauseAction;
+        [SerializeField] private GameObject pauseUi;
+        [SerializeField] private GameObject settingsUi;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Button continueButton;
+        [SerializeField] private Button returnToPauseButton;
+
+        private void Awake()
+        {
+            settingsButton.onClick.AddListener(ActivateSettings);
+            continueButton.onClick.AddListener(Continue);
+            returnToPauseButton.onClick.AddListener(ActivatePauseMenu);
+
+            pauseUi.SetActive(false);
+            settingsUi.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (pauseAction.action.WasPressedThisFrame())
+            {
+                if (settingsUi.activeSelf)
+                {
+                    ActivatePauseMenu();
+                }
+                else if (pauseUi.activeSelf)
+                {
+                    Continue();
+                }
+                else
+                {
+                    ActivatePauseMenu();
+                }
+            }
+        }
+
+        private void Continue()
+        {
+            pauseUi.SetActive(false);
+            settingsUi.SetActive(false);
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void ActivateSettings()
+        {
+            pauseUi.SetActive(false);
+            settingsUi.SetActive(true);
+        }
+
+        private void ActivatePauseMenu()
+        {
+            settingsUi.SetActive(false);
+            pauseUi.SetActive(true);
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
 }
